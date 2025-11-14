bdf88963a0b62704c80443b477934e86b16bc5c0
ryanpsouza <ryanpsouzagit@gmail.com>
2025-11-13 23:16:23 -0300

    feat: implementando server bem basico

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,23 @@
 import http from 'http';
 
+
+const users = [];
+
 const server = http.createServer((req, res) => {
-  res.end('Hello World!');
+  const { method, url } = req;
+
+  if(method === 'POST' && url === '/users' ){
+    users.push({
+      id: 1,
+      name: 'bob',
+      email: 'bob@example.com'
+    });
+
+    res.setHeader('Content-Type', 'application/json');
+    return res.end(JSON.stringify(users));
+  }
+
+  return res.end('Hello World!');
 });
 
 server.listen(3333);
