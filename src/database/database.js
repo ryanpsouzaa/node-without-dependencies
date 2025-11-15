86f26c86e05b045420ff035219ffc795f5590194
ryanpsouza <ryanpsouzagit@gmail.com>
2025-11-15 17:15:56 -0300

    feat: implementando logica do database

---
+++ src/database/database.js
@@ -0,0 +1,23 @@
+export class Database{
+
+  #database = {}
+
+  select(table){
+    return this.#database[table] ?? []
+  }
+
+  insert(table, data){
+    if(Array.isArray(this.#database[table])){
+      this.#database[table].push(data);
+    
+    } else {
+      this.#database[table] = [data];
+    }
+
+    return data;
+  }
+  
+  size(table){
+    return this.#database[table] ? this.#database[table].size : 0
+  }
+}
--- src/server/server.js
+++ src/server/server.js
@@ -1,37 +1,31 @@
 import http from 'http';
+import { json } from '../middlewares/json.js';
+import { Database } from '../database/database.js';
+import { randomUUID } from 'crypto';
 
-
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  const buffers = [];
-
-  for await (const chunk of req){
-    buffers.push(chunk)
-  }
-
-  try{
-    req.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    req.body = null;
-  }
+  await json(req, res);
 
   if(method === 'POST' && url === '/users' ){
     const { name, email } = req.body;
-    users.push({
-      id: 1,
+
+    const user = {
+      id: randomUUID(),
       name: name,
       email: email
-    });
+    }
+    database.insert('users', user)
 
-    res.setHeader('Content-Type', 'application/json');
-    return res.writeHead(201).end(JSON.stringify(users));
+    return res.writeHead(201).end(JSON.stringify(user));
 
   } else if(method === 'GET' && url === '/users' ){
-    res.setHeader('Content-Type', 'application/json');
+    const users = database.select('users');
     return res.writeHead(200).end(JSON.stringify(users));
+
   }
 
   return res.writeHead(404).end('Not Found');
