d569c4345d987daa601860da6317404350dd4b3d
ryanpsouza <ryanpsouzagit@gmail.com>
2025-11-15 15:27:54 -0300

    feat: implementando streams em modulos exemplos

---
+++ streams/streamHttpServer.js
@@ -0,0 +1,23 @@
+import http from 'node:http';
+import { Transform } from 'node:stream';
+
+class InverseNumberStream extends Transform{
+  
+    _transform(chunk, encoding, callback){
+      const transformed = Number(chunk.toString()) * -1
+
+      console.log(transformed);
+      
+      //primeiro parametro eh uma function/exception caso a transformacao do lote de algum erro
+      callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+const server = http.createServer((req, res) => {
+
+  return req
+    .pipe(new InverseNumberStream())
+    .pipe(res)
+});
+
+server.listen(3334);
