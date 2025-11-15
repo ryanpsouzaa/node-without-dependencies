736949c69809ccf581411988e3696c26eadc51d4
ryanpsouza <ryanpsouzagit@gmail.com>
2025-11-15 19:38:43 -0300

    feat: persistencia do banco de dados core implementada

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,23 @@
+import fs from 'node:fs/promises';
+
+const DATABASE_PATH = new URL('db.json', import.meta.url); 
+
 export class Database{
 
   #database = {}
+
+  constructor(){
+    fs.readFile(DATABASE_PATH, 'utf-8').then(data => {
+      this.#database = JSON.parse(data);
+      
+    }).catch(() => {
+      this.#persist()
+    })
+  }
+
+  #persist(){
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database));  
+  }
 
   select(table){
     return this.#database[table] ?? []
@@ -14,10 +31,8 @@ export class Database{
       this.#database[table] = [data];
     }
 
+    this.#persist();
+
     return data;
-  }
-  
-  size(table){
-    return this.#database[table] ? this.#database[table].size : 0
   }
 }
